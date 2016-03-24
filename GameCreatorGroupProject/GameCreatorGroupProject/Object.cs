5af3b77bf69bd32f0a60172de019d14ea45f7384
princessbobsanta <princessbobsantaclaus@gmail.com>
2016-03-23 19:28:12 -1000

    n-gon collision detection

--- GameCreatorGroupProject/GameCreatorGroupProject/Object.cs
+++ GameCreatorGroupProject/GameCreatorGroupProject/Object.cs
@@ -5,74 +5,502 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using OpenTK.Input;
-
-
+using System.Windows.Forms;
 
 namespace GameCreatorGroupProject
 {
     class GameObject
     {
+        //contains all gameObjects with collision detection on, might have to move to external class rather than static var in case user is working on more than one project
+        private static List<GameObject> collision = new List<GameObject>();
+        private bool col;
 
-        String objectname;
-        double x;
-        double y;
-        double speed;
+        private String objectname;
+        private double speed;
+        private double baseSpeed;
+        private double acc;
         //for map array, 0 max x value, 1 min x value, 2 max y value, 3 min y value
 
         double[] map = new double[4];
-        
 
-        //constructor takes as inputs, the name of the object, spawn location, and map details as an array of doubles that holds the maps size. The final value is the speed of the object.
-        public GameObject(String name, double xspawn, double yspawn, double[] inputmap, double ispeed)
+        //indicates direction last moved
+        private bool uAcc;
+        private bool dAcc;
+        private bool rAcc;
+        private bool lAcc;
+        //location of objects vertices
+        private double[][] loc;
+        //line segments composing the object
+        private List<Segment> segs = new List<Segment>();
+        //minimum and maximum x, y vals
+        private double maxX;
+        private double minX;
+        private double maxY;
+        private double minY;
+
+        //indicates if object has been spawned
+        private bool isSpawned = false;
+
+
+        //constructor takes as inputs, the name of the object, spawn location, map details as an array of doubles that holds the maps size, the speed of the object, acceleration of the object, and whether object has collision.
+        //NOTE: spawCoords must be vertices in order, with the last vertex connecting to the first
+        public GameObject(String name, double[][] spawnCoords, double[] inputmap, double ispeed, double acceleration, bool collision)
         {
+            col = collision;
             objectname = name;
+            baseSpeed = ispeed;
             speed = ispeed;
-            //if given spawn location is valid, make the 
-            if (isvalid(xspawn, yspawn))
+            acc = acceleration;
+            map = inputmap;
+            if (collision)
             {
-                x = xspawn;
-                y = yspawn;
+                GameObject.collision.Add(this);
+            }
+            //tells user if spawn failed
+            if (!spawn(spawnCoords))
+            {
+                MessageBox.Show("Spawn failed.", "Could not spawn object, invalid coordinates.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //indicates if object was successfully spawned
+            else
+            {
+                isSpawned = true;
             }
         }
 
+        //attempts to spawn object with given coordinates, returns true if successful, else false
+        public virtual bool spawn(double[][] spawnCoords)
+        {
+            maxX = spawnCoords[0][0];
+            minX = spawnCoords[0][0];
+            maxY = spawnCoords[0][1];
+            minY = spawnCoords[0][1];
+            List<Segment> temp = new List<Segment>();
+            //checks if each coordinate valid and sets values
+            for (int i = 0; i < spawnCoords.GetLength(0); i++)
+            {
+                //if given spawn location is valid, make the object, else return false
+                if (!isvalid(spawnCoords[i][0], spawnCoords[i][1]))
+                {
+                    return false;
+                }
+                //creates segments from coordinates, connecting them together as vertices, with the last vertex connecting to the first
+                if (i + 1 < spawnCoords.GetLength(0))
+                {
+                    temp.Add(new Segment(spawnCoords[i][0], spawnCoords[i][1], spawnCoords[i + 1][0], spawnCoords[i + 1][1]));
+                }
+                else
+                {
+                    temp.Add(new Segment(spawnCoords[i][0], spawnCoords[i][1], spawnCoords[0][0], spawnCoords[0][1]));
+                }
+                //finds maximum x and y values
+                if (spawnCoords[i][0] > maxX)
+                {
+                    maxX = spawnCoords[i][0];
+                }
+                if (spawnCoords[i][0] < minX)
+                {
+                    minX = spawnCoords[i][0];
+                }
+                if (spawnCoords[i][1] > maxY)
+                {
+                    maxY = spawnCoords[i][1];
+                }
+                if (spawnCoords[i][1] < minY)
+                {
+                    minY = spawnCoords[i][1];
+                }
+            }
+            //checks if intersects with any existing object
+            if (intersects(0, "l", temp))
+            {
+                return false;
+            }
+            //if all conditions met, sets object params and returns true
+            isSpawned = true;
+            segs = new List<Segment>(temp);
+            return true;
+
+        }
+
         //Update should check for user input at the start or end of each frame.a
-        void Update() {
-            //get state of all keyboards on device
-            var state = OpenTK.Input.Keyboard.GetState();
-            //checks up key, if it is pressed it will check if location is valid then update location.
-            if (state[Key.Up]) {
-                if (isvalid(x, y + speed))
-                    y = y + speed;
-
-            }
-            if (state[Key.Down])
+        public virtual void Update() {
+            //does nothing if object not spawned
+            if (isSpawned)
             {
-                if (isvalid(x, y - speed))
-                    y = y - speed;
+                //get state of all keyboards on device
+                var state = OpenTK.Input.Keyboard.GetState();
+                //checks up key, if it is pressed it will check if location is valid then update location.
+                if (state[Key.Up])
+                {
+                    //checks if accelerating and adds speed if it is
+                    if (uAcc)
+                    {
+                        speed += acc;
+                    }
+                    else
+                    {
+                        speed = baseSpeed;
+                    }
+                    bool valid = true;
+                    //checks if the new coordinates are valid
+                    foreach (double[] c in loc)
+                    {
+                        double x = c[0];
+                        double y = c[1];
+                        //checks if x and y coordinates will be valid after movement
+                        if (!isvalid(x, y + speed) || intersects(speed, "u", segs))
+                        {
+                            valid = false;
+                            break;
+                        }    
+                    }
+                    //modifies object location if valid
+                    if (valid)
+                    {
+                        //updates each relevant coordinate and segment location
+                        for (int i = 0; i < loc.GetLength(0); i++)
+                        {
+                            loc[i][1] += speed;
+                            segs[i].StartY += speed;
+                            segs[i].EndY += speed;
+                        }
+                    }
+                    //indicates that other directions are not accelerating
+                    dAcc = false;
+                    rAcc = false;
+                    lAcc = false;
+                }
+                //checks if down key is pressed (see above comments)
+                if (state[Key.Down])
+                {
+                    if (dAcc)
+                    {
+                        speed += acc;
+                    }
+                    else
+                    {
+                        speed = baseSpeed;
+                    }
+                    bool valid = true;
+                    foreach (double[] c in loc)
+                    {
+                        double x = c[0];
+                        double y = c[1];
 
+                        if (!isvalid(x, y - speed) || intersects(speed, "d", segs))
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid)
+                    {
+                        for (int i = 0; i < loc.GetLength(0); i++)
+                        {
+                            loc[i][1] -= speed;
+                            segs[i].StartY -= speed;
+                            segs[i].EndY -= speed;
+                        }
+                    }
+
+                    uAcc = false;
+                    rAcc = false;
+                    lAcc = false;
+
+                }
+                //checks if left key is pressed (see above comments)
+                if (state[Key.Left])
+                {
+                    if (lAcc)
+                    {
+                        speed += acc;
+                    }
+                    else
+                    {
+                        speed = baseSpeed;
+                    }
+                    bool valid = true;
+                    foreach (double[] c in loc)
+                    {
+                        double x = c[0];
+                        double y = c[1];
+
+                        if (!isvalid(x - speed, y) || intersects(speed, "l", segs))
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid)
+                    {
+                        for (int i = 0; i < loc.GetLength(0); i++)
+                        {
+                            loc[i][0] -= speed;
+                            segs[i].StartX -= speed;
+                            segs[i].EndX -= speed;
+                        }
+                    }
+
+                    uAcc = false;
+                    rAcc = false;
+                    dAcc = false;
+
+                }
+                //checks if right key is pressed (see above comments)
+                if (state[Key.Right])
+                {
+                    if (rAcc)
+                    {
+                        speed += acc;
+                    }
+                    else
+                    {
+                        speed = baseSpeed;
+                    }
+                    bool valid = true;
+                    foreach (double[] c in loc)
+                    {
+                        double x = c[0];
+                        double y = c[1];
+
+                        if (!isvalid(x + speed, y) || intersects(speed, "r", segs))
+                        {
+                            valid = false;
+                            break;
+                        }
+                    }
+                    if (valid)
+                    {
+                        for (int i = 0; i < loc.GetLength(0); i++)
+                        {
+                            loc[i][0] += speed;
+                            segs[i].StartX += speed;
+                            segs[i].EndX += speed;
+                        }
+                    }
+
+                    uAcc = false;
+                    lAcc = false;
+                    dAcc = false;
+
+                }
             }
-            if (state[Key.Left])
+
+        }
+
+        //chacks if segments in seg intersect with any collidable objects after given movement
+        private bool intersects(double speed, string dir, List<Segment> seg)
+        {
+            //this object has no collision
+            if (!col)
             {
-                if (isvalid(x-speed, y))
-                    x = x - speed;
-
+                return false;
             }
-            if (state[Key.Right])
+            //copies given list
+            List<Segment> temp = new List<Segment>(seg);
+            //modifies segment in temp based on movement
+            foreach (Segment s in temp)
             {
-                if (isvalid(x+speed, y))
-                    x = x + speed;
-
+                //indicates movement in upwards direction
+                if (dir.Equals("u"))
+                {
+                    //modifies segments accordingly
+                    s.StartY += speed;
+                    s.EndY += speed;
+                    //iterates through collidable objects
+                    foreach (GameObject o in collision)
+                    {
+                        //compares with each collidable objects segments
+                        foreach (Segment l in o.getSegments())
+                        {
+                            //chacks if intersect
+                            if (s.intersect(l))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                //indicates movement in downward direction (see above comments)
+                else if (dir.Equals("d"))
+                {
+                    s.StartY -= speed;
+                    s.EndY -= speed;
+                    foreach (GameObject o in collision)
+                    {
+                        foreach (Segment l in o.getSegments())
+                        {
+                            if (s.intersect(l))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                //indicates movement in left direction (see above comments)
+                else if (dir.Equals("l"))
+                {
+                    s.StartX -= speed;
+                    s.EndX -= speed;
+                    foreach (GameObject o in collision)
+                    {
+                        foreach (Segment l in o.getSegments())
+                        {
+                            if (s.intersect(l))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                //indicates movement in right direction (see above comments)
+                else if (dir.Equals("r"))
+                {
+                    s.StartX += speed;
+                    s.EndX += speed;
+                    foreach (GameObject o in collision)
+                    {
+                        foreach (Segment l in o.getSegments())
+                        {
+                            if (s.intersect(l))
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("invalid direction");
+                }
             }
-
+            //returns false if no collision detected
+            return false;
         }
 
 
         //check if location is valid, x coordinate, and y coordinate, max and min xy are the map boundaries
-        bool isvalid(double xc, double yc) {
-            if (xc < map[0] && xc > map[1] && yc < map[2] && yc > map[3]) {
-                return true;
+        public virtual bool isvalid(double xc, double yc) {
+            if (!(xc < map[0] && xc > map[1] && yc < map[2] && yc > map[3]))
+            {
+                return false;
             }
-            return false;
+            return true;
+            
+        }
+
+        //sets collision on and adds to collidable object list if not already in it
+        public void setCollision()
+        {
+            col = true;
+            if(!collision.Contains(this))
+            {
+                collision.Add(this);
+            }
+        }
+
+        //sets collision off and removes from collidable object list
+        public void removeCollision()
+        {
+            col = false;
+            collision.Remove(this);
+        }
+
+        //returns collision status
+        public bool getCol()
+        {
+            return col;
+        }
+
+
+        //getter and setter methods, many getter and setter methods
+        public String getName()
+        {
+            return objectname;
+        }
+
+        public void setName(String name)
+        {
+            objectname = name;
+        }
+
+        public double[][] getLoc()
+        {
+            return loc;
+        }
+
+        public double[] getMap()
+        {
+            return map;
+        }
+
+        public void setMap(double[] map)
+        {
+            this.map = map;
+        }
+
+        public double getAcc()
+        {
+            return acc;
+        }
+
+        public void setAcc(double acceleration)
+        {
+            acc = acceleration;
+        }
+
+        public double getBaseSpeed()
+        {
+            return baseSpeed;
+        }
+
+        public void setBaseSpeed(double ispeed)
+        {
+            baseSpeed = ispeed;
+        }
+
+        public double getSpeed()
+        {
+            return speed;
+        }
+
+        public void setSpeed(double speed)
+        {
+            this.speed = speed;
+        }
+
+
+
+        public double getMaxX()
+        {
+            return maxX;
+        }
+
+        public double getMinX()
+        {
+            return minX;
+        }
+
+        public double getMaxY()
+        {
+            return maxY;
+        }
+
+        public double getMinY()
+        {
+            return minY;
+        }
+
+
+        public bool spawned()
+        {
+            return isSpawned;
+        }
+
+        public List<Segment> getSegments()
+        {
+            return segs;
         }
     }
 
---
+++ GameCreatorGroupProject/GameCreatorGroupProject/Segment.cs
@@ -0,0 +1,169 @@
+﻿namespace GameCreatorGroupProject
+{
+    internal class Segment
+    {
+        private double startX;
+        private double endX;
+        private double startY;
+        private double endY;
+        private double m;
+        private double b;
+        private double lX;
+        private double hX;
+
+        private bool vert = false;
+
+        //creates a 2d segment with the specified start and end points
+        public Segment(double startX, double startY, double endX, double endY)
+        {
+            double run = startX - endX;
+            double rise = startY - endY;
+            //if equation valid, sets segments equation
+            if(run != 0)
+            {
+                m = rise / run;
+                b = startY - m * startX;
+            }
+            //else indicates vertical line
+            else
+            {
+                m = 0;
+                b = 0;
+                vert = true;
+            }
+
+            this.startX = startX;
+            this.endX = endX;
+            this.startY = startY;
+            this.endY = endY;
+            //sets highest and lowest x values
+            if (startX < endX)
+            {
+                lX = startX;
+                hX = endX;
+            }
+            else
+            {
+                hX = startX;
+                lX = endX;
+            }
+        }
+
+        //getter and setters
+        public double StartX
+        {
+            get
+            {
+                return startX;
+            }
+
+            set
+            {
+                startX = value;
+            }
+        }
+
+        public double EndX
+        {
+            get
+            {
+                return endX;
+            }
+
+            set
+            {
+                endX = value;
+            }
+        }
+
+        public double StartY
+        {
+            get
+            {
+                return startY;
+            }
+
+            set
+            {
+                startY = value;
+            }
+        }
+
+        public double EndY
+        {
+            get
+            {
+                return endY;
+            }
+
+            set
+            {
+                endY = value;
+            }
+        }
+
+        public double getM()
+        {
+            return m;
+        }
+
+        public double getB()
+        {
+            return b;
+        }
+
+        //checks if segments intersect on given interval
+        public bool intersect(Segment inter)
+        {
+            double interX;
+            //returns false if lines are parallel
+            if (inter.getM() == m && inter.getB() == b)
+            {
+                return false;
+            }
+            //if one of the segments is verticle, chacks if second segment intersects it
+            if (vert)
+            {
+                if (startX < inter.getHX() && startX > inter.getLX())
+                {
+                    return true;
+                }
+            }
+            else if (inter.isVert())
+            {
+                if (inter.startX < hX && inter.startX > lX)
+                {
+                    return true;
+                }
+            }
+            //else determines point of intersection based on m and b values and checks if intersection on interval bounded by the segments
+            else
+            {
+                interX = (b - inter.b) / (inter.m - m);
+                if (interX > lX && interX < hX)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+        //more getter and setter methods
+        public bool isVert()
+        {
+            return vert;
+        }
+
+        public double getHX()
+        {
+            return hX;
+        }
+
+        public double getLX()
+        {
+            return lX;
+        }
+
+    }
+}
