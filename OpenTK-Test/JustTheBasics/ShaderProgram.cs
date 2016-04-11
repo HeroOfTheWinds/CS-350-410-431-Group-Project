c1e43313796c03ed83d6af5ac2fd12d8b8fe4038
Chris N <swordsman9613@gmail.com>
2016-04-10 14:42:34 -1000

    Flesh out the rendering code with more and better comments

--- OpenTK-Test/JustTheBasics/ShaderProgram.cs
+++ OpenTK-Test/JustTheBasics/ShaderProgram.cs
@@ -10,6 +10,7 @@ using System.IO;
 
 namespace JustTheBasics
 {
+    // Class to serve as a template holding details about shader attributes
     public class AttributeInfo
     {
         public String name = "";
@@ -18,6 +19,7 @@ namespace JustTheBasics
         public ActiveAttribType type;
     }
 
+    // Class holding pertinent data about Uniforms in the shader
     public class UniformInfo
     {
         public String name = "";
@@ -26,27 +28,39 @@ namespace JustTheBasics
         public ActiveUniformType type;
     }
 
+    // This class handles the assembly and loading of GLSL (Graphics Language Shading Language) code,
+    // to be applied to the visuals in the game.
     class ShaderProgram
     {
-        public int ProgramID = -1;
-        public int VShaderID = -1;
-        public int FShaderID = -1;
+        // Create variables storing ID numbers of each component
+        public int ProgramID = -1; // The complete shader program ID
+        public int VShaderID = -1; // ID of a component vertex shader (how to map to vertices)
+        public int FShaderID = -1; // ID of a component fragment shader (how to color in the map)
+
+        // Variables to store how many attributes and Uniforms are in this shader
         public int AttributeCount = 0;
         public int UniformCount = 0;
 
+        // Create dictionaries to store the various attributes and Uniforms
         public Dictionary<String, AttributeInfo> Attributes = new Dictionary<string, AttributeInfo>();
         public Dictionary<String, UniformInfo> Uniforms = new Dictionary<string, UniformInfo>();
+        // Store a collection of unsigned-int buffer ideas, calling them by name
         public Dictionary<String, uint> Buffers = new Dictionary<string, uint>();
 
+        // This function has OpenTK create a new shader Program ID, and stores in in the class
         public ShaderProgram()
         {
             ProgramID = GL.CreateProgram();
         }
 
+        // This function does the same as above, but also takes a vertex shader 
+        // and a fragment shader as arguments, either completely in String form
+        // or as file names to be loaded (provided fromFile is true)
         public ShaderProgram(String vshader, String fshader, bool fromFile = false)
         {
             ProgramID = GL.CreateProgram();
 
+            // If we are using file names, use a helper function to load the shaders
             if (fromFile)
             {
                 LoadShaderFromFile(vshader, ShaderType.VertexShader);
@@ -54,23 +68,33 @@ namespace JustTheBasics
             }
             else
             {
+                // Else, use a helper function for string loading
                 LoadShaderFromString(vshader, ShaderType.VertexShader);
                 LoadShaderFromString(fshader, ShaderType.FragmentShader);
             }
 
+            // Link the shaders to the program
             Link();
+            // Generate buffers to store shading information
             GenBuffers();
         }
 
+        // This function takes shader code and info on whether it is vertex or fragment type
+        // Returns the value in an int passed by reference
         private void loadShader(String code, ShaderType type, out int address)
         {
+            // Generate an ID for referencing the new shader
             address = GL.CreateShader(type);
+            // Tell OpenTK what the source code for the shader should be
             GL.ShaderSource(address, code);
+            // Compile it from the GLSL code
             GL.CompileShader(address);
+            // Link it into our shader program
             GL.AttachShader(ProgramID, address);
-            Console.WriteLine(GL.GetShaderInfoLog(address));
+            Console.WriteLine(GL.GetShaderInfoLog(address)); // print out success or failure
         }
 
+        // If a shader is passed as a string, call the loadShader function
         public void LoadShaderFromString(String code, ShaderType type)
         {
             if (type == ShaderType.VertexShader)
@@ -83,6 +107,7 @@ namespace JustTheBasics
             }
         }
 
+        // Passing in a file name, so load the file into a string before calling loadShader
         public void LoadShaderFromFile(String filename, ShaderType type)
         {
             using (StreamReader sr = new StreamReader(filename))
@@ -98,46 +123,62 @@ namespace JustTheBasics
             }
         }
 
+        // This function causes OpenTK to start using the given shader program.  It also generates the
+        // dictionary of attributes and Uniforms for the given program
         public void Link()
         {
+            // Have TK associate the Program with the current process
             GL.LinkProgram(ProgramID);
 
+            // Write any success or failure information
             Console.WriteLine(GL.GetProgramInfoLog(ProgramID));
 
+            // Get the number of attributes and Uniforms in the program and store the numbers
             GL.GetProgram(ProgramID, GetProgramParameterName.ActiveAttributes, out AttributeCount);
             GL.GetProgram(ProgramID, GetProgramParameterName.ActiveUniforms, out UniformCount);
 
+            // For each attribute:
             for (int i = 0; i < AttributeCount; i++)
             {
+                // Initialize an AttributeInfo object
                 AttributeInfo info = new AttributeInfo();
                 int length = 0;
 
                 StringBuilder name = new StringBuilder();
 
+                // Get the current attribute's values and store them in the new object
                 GL.GetActiveAttrib(ProgramID, i, 256, out length, out info.size, out info.type, name);
 
+                // Set the last two fields
                 info.name = name.ToString();
                 info.address = GL.GetAttribLocation(ProgramID, info.name);
+                // Insert the object into our dictionary of attributes
                 Attributes.Add(name.ToString(), info);
             }
 
+            // For each Uniform:
             for (int i = 0; i < UniformCount; i++)
             {
+                // Create a new info object
                 UniformInfo info = new UniformInfo();
                 int length = 0;
 
                 StringBuilder name = new StringBuilder();
 
+                // Get the information out of the current Uniform
                 GL.GetActiveUniform(ProgramID, i, 256, out length, out info.size, out info.type, name);
 
+                // Set the rest and store it in our dictionary
                 info.name = name.ToString();
                 Uniforms.Add(name.ToString(), info);
                 info.address = GL.GetUniformLocation(ProgramID, info.name);
             }
         }
 
+        // Function for creating shader buffers on the GPU
         public void GenBuffers()
         {
+            // Generate a buffer for each attribute and store it in our dictionary of buffers
             for (int i = 0; i < Attributes.Count; i++)
             {
                 uint buffer = 0;
@@ -146,6 +187,7 @@ namespace JustTheBasics
                 Buffers.Add(Attributes.Values.ElementAt(i).name, buffer);
             }
 
+            // Do the same for each Uniform
             for (int i = 0; i < Uniforms.Count; i++)
             {
                 uint buffer = 0;
@@ -155,6 +197,7 @@ namespace JustTheBasics
             }
         }
 
+        // This function goes over every attribute in the program, and allows it to use attribute arrays
         public void EnableVertexAttribArrays()
         {
             for (int i = 0; i < Attributes.Count; i++)
@@ -163,6 +206,7 @@ namespace JustTheBasics
             }
         }
 
+        // Opposite of above, disables the use of arrays for all attributes
         public void DisableVertexAttribArrays()
         {
             for (int i = 0; i < Attributes.Count; i++)
@@ -170,6 +214,8 @@ namespace JustTheBasics
                 GL.DisableVertexAttribArray(Attributes.Values.ElementAt(i).address);
             }
         }
+
+        // Accessor functions to simplify dictionary access
 
         public int GetAttribute(string name)
         {
