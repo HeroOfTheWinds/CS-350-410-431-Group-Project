622ea71c7090821fa9d31c7214caf6b05d02d5a2
Kyle <aquaclouds@hotmail.com>
2016-04-19 20:21:58 -1000

    Adding ChatWindow designer

--- GameCreatorGroupProject/GameCreatorGroupProject/ChatWindow.Designer.cs
+++ GameCreatorGroupProject/GameCreatorGroupProject/ChatWindow.Designer.cs
@@ -71,6 +71,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(175, 97);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(553, 446);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
